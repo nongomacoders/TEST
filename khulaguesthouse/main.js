2f6651c2f91c26c24cb1a636f00108dbb163acff
asusdion <dr.dtdavan@gmail.com>
2020-11-04 13:57:05 +0200

    changing picture

--- khulaguesthouse/main.js
+++ khulaguesthouse/main.js
@@ -1,18 +1,19 @@
-let picsArray = ["pic1", "pic2", "pic3"];
+let picsArray = ["./assets/pics/pic1", "./assets/pics/pic2", "./assets/pics/pic3"];
 
 let currentPicNo = 0;
 
-console.log(picsArray[currentPicNo]);
+let mypic = document.getElementById("myimg");
 
-nextPic();
-nextPic();
 
 
 function nextPic() {
-    currentPicNo = 1;// change this line
+    currentPicNo = currentPicNo + 1;// change this line
+    mypic.src = picsArray[currentPicNo];
     console.log(picsArray[currentPicNo]);
 }
 
 function prevPic() {
+    currentPicNo = currentPicNo - 1;
+    mypic.src = picsArray[currentPicNo];
     console.log(picsArray[currentPicNo])
 }
